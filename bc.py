1c88a83a2e841719d5ebc3e0d614b22e610a9ee3
Dennis Kwong <denkwong@gmail.com>
2018-11-13 15:21:47 -0800

    Rewrite of bc. Can search on ability, cost, effect, name, rarity, and target.

---
+++ bc.py
@@ -0,0 +1,152 @@
+# created by: Dennis Kwong
+# cost: 2.00 cat food
+
+import json
+import logging
+import re
+from cat import Cat
+
+
+class Bc:
+    def __init__(self):
+        """
+        open bc.json and load into self.cats.
+        """
+        with open("bc.json", "r") as fh:
+            self.json_cats = json.load(fh)
+            self.cats = list()
+            self.load_cats()
+
+    def find_ability(self, ability, cats=None):
+        """
+        find cat with ability
+        :param str ability: name of ability, can be regex
+        :param list cats: optional list of Cats to search
+        :return: list of Cats
+        :rtype: list
+        """
+        result = list()
+        if cats is None:
+            cats = self.cats
+        for cat in cats:
+            matches = cat.get_ability(ability)
+            if matches:
+                result.append(cat)
+        return result
+
+    def find_cat(self, name, cats=None):
+        """
+        find cat with name
+        :param str name: name of cat, can be regex
+        :param list cats: optional list of Cats to search
+        :return: list of Cats
+        :rtype: list
+        """
+        result = list()
+        if cats is None:
+            cats = self.cats
+        for cat in cats:
+            cat_name = cat.get_name()
+            matches = re.search(name, cat_name, re.IGNORECASE)
+            if matches:
+                result.append(cat)
+        return result
+
+    def find_cost(self, cost, cats=None):
+        """
+        find cost of cat, also supports operators <, <=, >=, >
+        :param int or str cost: examples supported: 75, ">= 75", "< 1000"
+        :param list cats: optional list of Cats to search
+        :return: list of Cats
+        :rtype: list
+        """
+        operator = "=="
+        if cats is None:
+            cats = self.cats
+
+        if len(str(cost).split(" ")) > 1:
+            operator = cost.split(" ")[0]
+            cost = cost.split(" ")[1]
+
+        result = self._find_cost(int(cost), operator, cats)
+        return result
+
+    def _find_cost(self, cost, operator, cats):
+        """
+        helper method that handles cost search with supported operators
+        :param int cost: cost of Cat
+        :param str operator: integer equality / inequality operators
+        :param list cats: list of Cats to search
+        :return: list of Cats matching the search criteria
+        :rtype: list
+        """
+        if operator in ("<", "<=", "==", ">=", ">"):
+            result = list()
+            for cat in cats:
+                cat_cost = cat.get_cost()
+                if eval("{0} {1} {2}".format(cat_cost, operator, cost)):
+                    result.append(cat)
+            return result
+        else:
+            print("Operator {0} is not supported.".format(operator))
+
+    def find_effect(self, effect, cats=None):
+        """
+        find cat with effect
+        :param str effect: name of effect, can be regex
+        :param list cats: optional list of Cats to search
+        :return: list of Cats
+        :rtype: list
+        """
+        result = list()
+        if cats is None:
+            cats = self.cats
+        for cat in cats:
+            matches = cat.get_effect(effect)
+            if matches:
+                result.append(cat)
+        return result
+
+    def find_rarity(self, rarity, cats=None):
+        """
+        return a list of cats that match specified rarity
+        :param str rarity: normal, special, rare, super, uber
+        :param list cats: optional list of Cats to search
+        :return: list of Cats that match rarity
+        :rtype: list
+        """
+        result = list()
+        if cats is None:
+            cats = self.cats
+        for cat in cats:
+            matches = re.search(rarity, cat.get_rarity(), re.IGNORECASE)
+            if matches:
+                result.append(cat)
+        return result
+
+    def find_target(self, target, cats=None):
+        """
+        return a list of cats with the specified target
+        :param str target: case-insensitive Black, Floating, etc.
+        :param list cats: optional list of Cats to search
+        :return: list of Cats that match target
+        :rtype: list
+        """
+        logging.info("find_target(target={0}, cats={1})".format(target, cats))
+        result = list()
+        if cats is None:
+            cats = self.cats
+        for cat in cats:
+            matches = cat.get_target(target)
+            if matches:
+                result.append(cat)
+        logging.debug("find_target len(result)={0} result={1}".format(len(result), result))
+        return result
+
+    def load_cats(self):
+        """
+        read cats from bc.json, create Cat objects and load them into self.cats.
+        :return: None
+        """
+        for cat in self.json_cats["cats"]:
+            self.cats.append(Cat(cat))
---
+++ cat.py
@@ -0,0 +1,99 @@
+# created by: Dennis Kwong
+# cost: 2.00 cat food
+
+import re
+
+
+class Cat:
+    def __init__(self, cat):
+        self.cat = cat
+        self.ability = cat["ability"]
+        self.alias = cat["alias"]
+        self.cost = cat["cost"]
+        self.description = cat["description"]
+        self.effect = cat["effect"]
+        self.form = cat["form"]
+        self.name = cat["name"]
+        self.rarity_index = cat["rarity_index"]
+        self.rarity_pct = cat["rarity_pct"]
+        self.rarity_total = cat["rarity_total"]
+        self.rarity = cat["rarity"]
+        self.target = cat["target"]
+
+    def get_ability(self, search=None):
+        """
+        return list of abilities or abilities that match search regex
+        :param str search: optional regex search string
+        :return: list of abilities
+        :rtype: list
+        """
+        if search:
+            result = list()
+            for ability in self.ability:
+                matches = re.search(search, ability, re.IGNORECASE)
+                if matches:
+                    result.append(ability)
+        else:
+            result = self.ability
+        return result
+
+    def get_alias(self):
+        return self.alias
+
+    def get_cost(self):
+        return self.cost
+
+    def get_description(self):
+        return self.description
+
+    def get_effect(self, search=None):
+        """
+        return list of effects or effects that match search regex
+        :param str search: optional regex search string
+        :return: list of effects
+        :rtype: list
+        """
+        if search:
+            result = list()
+            for effect in self.effect:
+                matches = re.search(search, effect, re.IGNORECASE)
+                if matches:
+                    result.append(effect)
+        else:
+            result = self.effect
+        return result
+
+    def get_form(self):
+        return self.form
+
+    def get_name(self):
+        return self.name
+
+    def get_rarity(self):
+        return self.rarity
+
+    def get_rarity_index(self):
+        return self.rarity_index
+
+    def get_rarity_pct(self):
+        return self.rarity_pct
+
+    def get_rarity_total(self):
+        return self.rarity_total
+
+    def get_target(self, search=None):
+        """
+        return list of targets or targets that match search regex
+        :param str search: optional regex search string
+        :return: list of targets
+        :rtype: list
+        """
+        if search:
+            result = list()
+            for target in self.target:
+                matches = re.search(search, target, re.IGNORECASE)
+                if matches:
+                    result.append(target)
+        else:
+            result = self.target
+        return result
